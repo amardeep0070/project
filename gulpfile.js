e8be72b7fee2d88909ff5c44d9606f53839ca25a
amardeep0070 <amardeep007-@gmail.com>
2016-11-08 16:16:36 -0500

    Added Gulp task for heroku#1

--- gulpfile.js
+++ gulpfile.js
@@ -103,3 +103,6 @@ gulp.task('sass', function() {
             stream: true
         }))
 });
+gulp.task("heroku:production", function(){
+    console.log('hello'); // the task does not need to do anything.
+});
